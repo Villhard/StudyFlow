4d8b877ecf9724ccc59b6f4debbceb3d945e5f7a
Victor Ryabov <victorwillhard@gmail.com>
2024-02-10 17:50:41 +0300

    add bootstrap files and delete default comments

--- studyflow/cards/urls.py
+++ studyflow/cards/urls.py
@@ -1,6 +1,8 @@
-from cards.views import CardAPIView
 from django.urls import path
 
+from .views import CardAPIView, index
+
 urlpatterns = [
-    path("api/v1/cards/", CardAPIView.as_view()),
+    path("", index, name="index"),
+    path("api/v1/cards/", CardAPIView.as_view(), name="api-v1-cards"),
 ]
--- studyflow/cards/views.py
+++ studyflow/cards/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import generics
 
 from .models import Card
@@ -7,3 +8,8 @@ from .serializers import CardSerializer
 class CardAPIView(generics.ListCreateAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+
+
+def index(request):
+    context = {"cards": Card.objects.all()}
+    return render(request, "cards/index.html", context)
