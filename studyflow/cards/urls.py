92797a7baf3d27c6a6f0b0ff431867fb4f4daea4
Victor Ryabov <victorwillhard@gmail.com>
2024-02-15 17:29:19 +0300

    delete all about add card

--- studyflow/cards/urls.py
+++ studyflow/cards/urls.py
@@ -6,10 +6,6 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("cards/", views.view_cards, name="cards"),
     path("cards/<int:card_id>", views.view_card, name="card"),
-    path("cards/add_card/", views.add_card, name="add-card"),
-]
-
-# API
-urlpatterns += [
+    # API
     path("api/v1/cards/", views.CardAPIView.as_view(), name="api-v1-cards"),
 ]
--- studyflow/cards/views.py
+++ studyflow/cards/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from rest_framework import generics
 
-from .forms import AddCardForm
 from .models import Card
 from .serializers import CardSerializer
 
@@ -20,17 +19,6 @@ def view_card(request, card_id):
     card = get_object_or_404(Card, id=card_id)
     context = {"card": card}
     return render(request, "cards/card.html", context)
-
-
-def add_card(request):
-    form = AddCardForm()
-    if request.method == "POST":
-        form = AddCardForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-    context = {"form": form}
-    return render(request, "cards/add_card.html", context)
 
 
 # API
