0fa73c300bd35ce85daeb5c30ce80685e5a0f56f
Victor Ryabov <victorwillhard@gmail.com>
2024-02-18 13:23:21 +0300

    add 13 level knowledge

--- studyflow/cards/models.py
+++ studyflow/cards/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator
 from django.db import models
 from django.utils import timezone
 
@@ -8,8 +9,12 @@ User = get_user_model()
 class Card(models.Model):
     front_side = models.TextField()
     back_side = models.TextField()
-    next_time = models.DateTimeField(default=timezone.now)
-    knowledge_level = models.PositiveSmallIntegerField(default=0)
+    next_time = models.DateTimeField(
+        default=timezone.now, blank=True, null=True
+    )
+    knowledge_level = models.PositiveSmallIntegerField(
+        default=0, validators=[MaxValueValidator(13)]
+    )
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="cards"
     )
